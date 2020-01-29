1e26d5eb844f9507ddf4eaa846b2170828697b83
Anastasiia Prodan <nastyaProdan100@gmail.com>
2020-01-29 07:06:48 +0200

    Initial project commit

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsbuddyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsbuddyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsbuddyApplication.class, args);
+    }
 
 }
