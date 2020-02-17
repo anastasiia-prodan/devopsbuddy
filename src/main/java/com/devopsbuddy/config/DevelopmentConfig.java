5375941c124d1c32431a2c3ea794d7d40b0629f1
Anastasiia Prodan <nastyaProdan100@gmail.com>
2020-02-17 06:47:59 +0200

    Set up the H2 console and created the repository layer

--- src/main/java/com/devopsbuddy/config/DevelopmentConfig.java
+++ src/main/java/com/devopsbuddy/config/DevelopmentConfig.java
@@ -2,10 +2,13 @@ package com.devopsbuddy.config;
 
 import com.devopsbuddy.backend.service.EmailService;
 import com.devopsbuddy.backend.service.MockEmailService;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+
 
 @Configuration
 @Profile("dev")
@@ -15,5 +18,12 @@ public class DevelopmentConfig {
     @Bean
     public EmailService emailService() {
         return new MockEmailService();
+    }
+
+    @Bean
+    public ServletRegistrationBean h2ConsoleServletRegistration() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new WebServlet());
+        bean.addUrlMappings("/console/*");
+        return bean;
     }
 }
